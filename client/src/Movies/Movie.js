6d3bab08c8bc89c61ce76ed40130e34c0a9d186d
lilyhoratio <lilyhzhou@gmail.com>
2019-07-23 15:03:05 -0700

    turned saved movie list into links

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -38,22 +38,7 @@ const Movie = (props) => {
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie}/>
-      {/* <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div> */}
+      {/* <div className="save-button" onClick={() => props.addToSavedList(movie)}>Save</div> */}
       <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
